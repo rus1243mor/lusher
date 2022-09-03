52e389892b95c9c6bce010aa569de18dd224c493
Jmail <gamelelkazzaz@gmail.com>
2022-09-03 12:26:15 +0300

    update

--- bot.js
+++ bot.js
@@ -8,26 +8,9 @@ const app = express();
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }))
-app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-app.get('/', async(req, res) => {
-    console.log(req.body)
+app.get('/results', async(req, res) => {
+    console.log(req.query)
     res.send("get it")
 });
 
